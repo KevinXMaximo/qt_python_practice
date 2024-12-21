39af5cbfac86a2e9c5b5e63a93f078c53004097b
KevinXMaximo <62119614+KevinXMaximo@users.noreply.github.com>
2024-12-21 11:02:20 -0500

    Refactored redundant code into ButtonHolder class

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 # QApplication handles the main GUI application settings and QMainWindow + QPushButton are widget classes
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton 
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+# CLI functionality 
+import sys
 
-# Commandline functionality
-import sys 
+# Button class inheriting from QMainWindow
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button Holder App")
+        # Instance of the button widget
+        button = QPushButton("Press Me!")
+
+        # Set up the button as the central widget
+        self.setCentralWidget(button)
 
 # Instance of QApplication with a list of command-line arguments
 app = QApplication(sys.argv) 
 
-# Instance of the MainWindow widget
-window = QMainWindow()
-window.setWindowTitle("My first MainWindow app")
-
-# Instance of the button widget
-button = QPushButton()
-button.setText("Press me")
-
-# Attatches button widget to the window
-window.setCentralWidget(button)
+# Instance of the ButtonHolder class
+window = ButtonHolder()
 
 # setVisible(true) and Main event loop until exit()
 window.show()
