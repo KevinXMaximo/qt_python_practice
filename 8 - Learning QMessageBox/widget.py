6e41c32ce16e824775c7e315ac595d38e4f46013
KevinXMaximo <62119614+KevinXMaximo@users.noreply.github.com>
2025-01-08 09:09:25 -0500

    Introduced code to track when a user highlights text

--- 8 - Learning QMessageBox/widget.py
+++ 8 - Learning QMessageBox/widget.py
@@ -11,7 +11,9 @@ class Widget(QWidget):
         self.line_edit = QLineEdit()
         #self.line_edit.textChanged.connect(self.text_changed)
         #self.line_edit.cursorPositionChanged.connect(self.cursor_position_changed)
-        self.line_edit.editingFinished.connect(self.editing_finished)
+        #self.line_edit.editingFinished.connect(self.editing_finished)
+        #self.line_edit.returnPressed.connect(self.return_pressed)
+        self.line_edit.selectionChanged.connect(self.selection_changed)
 
         button = QPushButton("Grab Data")
         button.clicked.connect(self.button_clicked)
@@ -42,3 +44,9 @@ class Widget(QWidget):
 
     def editing_finished(self) :
         print("Editing Finished")
+
+    def return_pressed(self):
+        print("Return pressed")
+
+    def selection_changed(self):
+        print("Selection Changed: ", self.line_edit.selectedText())
