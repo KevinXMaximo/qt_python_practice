17f4aa9a934666c8b10d8ce6cf786f0c0dc2d4e9
KevinXMaximo <62119614+KevinXMaximo@users.noreply.github.com>
2025-01-13 22:08:17 -0500

    Tested what happens if both x and y size policies are set to expanding

--- Tutorial 11 - Size Policies/widget.py
+++ Tutorial 11 - Size Policies/widget.py
@@ -10,7 +10,7 @@ class Widget(QWidget):
         label = QLabel("Some text : ")
         line_edit = QLineEdit()
 
-        line_edit.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        line_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         h_layout_1 = QHBoxLayout()
         h_layout_1.addWidget(label)
